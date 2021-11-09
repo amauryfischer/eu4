972f9a39c80886676cbd1f7468b66f91cf7a6282
Amaury Fischer <amaufischer@gmail.com>
2021-11-09 10:13:28 +0100

    :art: Improve resources reducer

--- app/javascript/app/redux/resources/resourcesReducer.ts
+++ app/javascript/app/redux/resources/resourcesReducer.ts
@@ -3,24 +3,26 @@ import { createSlice } from "@reduxjs/toolkit"
 export const TITANIUM = "TITANIUM"
 export const COPPER = "COPPER"
 export const IRON = "IRON"
-export const GOLD = "GOLD"
-export const PLATINUM = "PLATINUM"
-export const COAL = "COAL"
-export const URANIUM = "URANIUM"
 export const ALUMINUM = "ALUMINUM"
-export const SILVER = "SILVER"
+export const URANIUM = "URANIUM"
+export const MERCURE = "MERCURE"
+export const SILICIUM = "SILICIUM"
+export const KRYPTON = "KRYPTON"
+export const AZOTE = "AZOTE"
+export const HYDROGENE = "HYDROGENE"
 export const counterSlice = createSlice({
   name: "resources",
   initialState: {
     [TITANIUM]: 0,
     [COPPER]: 0,
     [IRON]: 0,
-    [COAL]: 0,
-    [URANIUM]: 0,
-    [GOLD]: 0,
-    [SILVER]: 0,
-    [PLATINUM]: 0,
     [ALUMINUM]: 0,
+    [MERCURE]: 0,
+    [SILICIUM]: 0,
+    [URANIUM]: 0,
+    [KRYPTON]: 0,
+    [AZOTE]: 0,
+    [HYDROGENE]: 0,
   },
   reducers: {
     addResource: (state, { payload: { type, amount } }) => {
