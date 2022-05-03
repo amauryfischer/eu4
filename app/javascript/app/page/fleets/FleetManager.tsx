b3363ff2ce0d83a2f12fe90d4746d00eb56d8731
Amaury Fischer <amaufischer@gmail.com>
2022-05-03 17:28:18 +0200

    go back on fleetmanager

--- app/javascript/app/page/fleets/FleetManager.tsx
+++ app/javascript/app/page/fleets/FleetManager.tsx
@@ -5,8 +5,9 @@ import styled from "styled-components"
 import useFleet from "hooks/useFleet"
 import Flex from "styles/Flex"
 import { YellowTextField } from "styles/testField"
-import { YellowButton } from "styles/button"
+import { YellowButton, YellowButtonOutlined } from "styles/button"
 import { updateFleet } from "reducer/fleets/fleetResource"
+import { useNavigate } from "react-router"
 
 // app:javascript:app:page:fleets:FleetManager.tsx
 const debug = Debug("app:javascript:app:page:fleets:FleetManager")
@@ -18,6 +19,7 @@ const FleetManager = ({}) => {
   const [x, setX] = useState(fleet?.data?.position?.systemPosition?.x)
   const [y, setY] = useState(fleet?.data?.position?.systemPosition?.y)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const moveFleet = () => {
     dispatch(
@@ -30,6 +32,9 @@ const FleetManager = ({}) => {
 
   return (
     <Flex direction="column">
+      <YellowButtonOutlined onClick={() => navigate(-1)}>
+        Retour
+      </YellowButtonOutlined>
       <h1>{fleet?.data?.name}</h1>
       <YellowTextField
         label="Systeme solaire"
